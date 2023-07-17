132ab0c5f7b23241fc45e2fbc2d78720d6822416
behzad dara <Behzad.dara.99@gmail.com>
2023-07-17 18:50:27 +0330

    Add project files.

---
+++ TestQuartz/Program.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+IScheduler scheduler = await schedulerFactory.GetScheduler();
+
+IJobDetail job = JobBuilder.Create<HelloWorldJob>()
+    .WithIdentity("helloJob", "group")
+    .Build();
+
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("helloTrigger", "group")
+    .StartNow()
+    /*.WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(5)
+        .RepeatForever())*/
+    .WithCronSchedule("*/5 * * ? * * *")
+    .Build();
+
+await scheduler.ScheduleJob(job, trigger);
+await scheduler.Start();
+
+while (true);
+
+public class HelloWorldJob : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"Hello world! {DateTime.Now:yyyy:MM:dd} - {DateTime.Now:HH:mm:ss}");
+        return Task.CompletedTask;
+    }
+}
